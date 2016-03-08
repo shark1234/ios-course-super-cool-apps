6c8491721bebe6bfb51c04ae63b09bceb74795e0
Aaron Hiller <quick_silver00@aarons-macbook-pro.local>
2016-03-07 21:59:20 -0500

    Initial Commit

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Aaron Hiller on 2016-03-07.
+//  Copyright © 2016 Aaron Hiller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
